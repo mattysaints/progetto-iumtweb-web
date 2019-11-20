02b840aaec4a54fe434608661061f7585d1e2e2f
Davide Zago <davide.zago@edu.unito.it>
2019-11-20 12:48:59 +0100

    Prova

--- src/servlet/Servlet.java
+++ src/servlet/Servlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Servlet")
+@WebServlet(name = "Servlet", urlPatterns = {"progetto-ium-tweb2/Servlet"})
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
