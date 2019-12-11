c7709b7f02ed01d710c7c241633b8c26b34d3283
Marco Scalerandi <scalerandi.marco@educ.di.unito.it>
2019-12-11 15:17:47 +0100

    risolto errore di merge...aveva comportato perdita di lavoro nel dao

--- src/project/servlet/OpSuPrenotazioni.java
+++ src/project/servlet/OpSuPrenotazioni.java
@@ -1,31 +1,67 @@
 package project.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import project.servlet.model.DAO;
+import project.servlet.model.Prenotazione;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "OpSuPrenotazioni")
+@WebServlet(name = "OpSuPrenotazioni", urlPatterns = {"/progetto_ium_tweb2/OpSuPrenotazioni"})
 public class OpSuPrenotazioni extends HttpServlet {
    /**
     * Si possono fare 3 operazioni:
     * - prenotare
     * - disdire
     * - effettuare
-    * una prenotazione.
+    * una ripetizione.
+    * Questo verrà discriminato da una variabile nella request (op).
     *
-    * Questo verrà discriminato da una variabile nella request (op)
     *
     */
 
+   /**
+    *
+    * @param request parametri: "op" assegnare uno tra "prenotare", "disdire" ed "effettuare"; "prenotazione" assegnare oggetto json corrispondente
+    * @param response
+    * @throws ServletException
+    * @throws IOException
+    */
+   private void esegui(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      String op = (String) request.getAttribute("op");
+      Gson json = new Gson();
+      Prenotazione prenot = json.fromJson((JsonElement) request.getAttribute("prenotazione"), Prenotazione.class);
+      DAO.registerDriver();
+      PrintWriter out = response.getWriter();
+      boolean correct = false;
+      switch (op) {
+         case "prenotare":
+            correct = DAO.addRip(prenot);
+            break;
+         case "disdire":
+            correct = DAO.disdireRip(prenot);
+            break;
+         case "effettuare":
+            correct = DAO.makeRip(prenot);
+            break;
+         default:
+            throw new ServletException("L'operazione richiesta non è tra quelle servite (scegliere tra 'prenotare', 'disdire', 'effettuare')");
+      }
+      out.print(correct);
+   }
 
    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+      esegui(request,response);
    }
 
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+      esegui(request,response);
    }
 }
--- src/project/servlet/StoricoPrenotazioni.java
+++ src/project/servlet/StoricoPrenotazioni.java
@@ -1,11 +1,19 @@
 package project.servlet;
 
+package project.servlet;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import project.servlet.model.DAO;
+import project.servlet.model.Utente;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "StoricoPrenotazioni")
 public class StoricoPrenotazioni extends HttpServlet {
@@ -17,12 +25,28 @@ public class StoricoPrenotazioni extends HttpServlet {
     *
     * Questo verrà discriminato da un campo nella richiesta (utente: se null si intende la richiesta di storico generale)
     */
-
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+   /**
+    *
+    * @param request parametro: "utente" in formato json, se null si intende lo storico generale
+    * @param response
+    * @throws ServletException
+    * @throws IOException
+    */
+   private void esegui(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      Gson json = new Gson();
+      Utente u = json.fromJson((JsonElement)request.getAttribute("utente"), Utente.class);
+      DAO.registerDriver();
+      response.setContentType("application/json");
+      PrintWriter out = response.getWriter();
+      if(u==null){
+         out.
+      }
    }
 
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      esegui(request,response);
+   }
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+      esegui(request,response);
    }
 }
