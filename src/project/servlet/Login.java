39d8c3bb359d1fb3f184277f2e0474f0b8008f18
Davide Zago <davide.zago@edu.unito.it>
2019-12-12 23:20:47 +0100

    Modifica del url della servlet Login e del ritorno sullo stream di output. Aggiunta degli attributi name nella form della loginPage.html in modo da ottenerli nella servlet Login

--- src/project/servlet/Login.java
+++ src/project/servlet/Login.java
@@ -1,5 +1,6 @@
 package project.servlet;
 
+import com.google.gson.Gson;
 import project.servlet.model.DAO;
 import project.servlet.model.Utente;
 
@@ -19,11 +20,15 @@ import java.io.PrintWriter;
  *  - username: username dell'utente che ha fatto il login
  *  - password: password utente
  *
+ *  Stampa:
+ *   - null se l'utente non è registrato
+ *   -l'oggetto session in formato Json
+ *
  *  Parametri della sessione:
  *  - username: username dell'utente associato alla sessione
  *  - admin: true se l'utente è admin
  */
-@WebServlet(name = "Login", urlPatterns = {"/progetto_ium_tweb2/Login"})
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
 public class Login extends HttpServlet {
     /**
      * Inizializza la servlet
@@ -48,17 +53,21 @@ public class Login extends HttpServlet {
      */
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         HttpSession session = request.getSession();
+        response.setContentType("application/json");
         PrintWriter out = response.getWriter();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        Utente utente = DAO.queryUtente(new Utente(username, password, null));
-        if (utente == null)
+        Utente utente = new DAO().queryUtente(new Utente(username, password, null));
+        if (utente == null) {
+            session.invalidate();
             out.println((String) null);
-        else {
+        } else {
             session.setAttribute("username", utente.getAccount());
             session.setAttribute("admin", utente.isAdmin());
-            out.println(session.getId());
+            Gson gson = new Gson();
+            String json = gson.toJson(session);
+            out.println(json);
         }
     }
 
